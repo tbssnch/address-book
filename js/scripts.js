7395b6c972ee2d4d2f6cccfcd654f1c0933366b1
Tyler Bussanich <tbussanich@gmail.com>
2017-10-31 11:51:09 -0700

    add street, city, state, and zip

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,11 @@
 //Business Logic
-function Contact(first, last) {
+function Contact(first, last, street, city, state, zip) {
   this.firstName = first;
   this.lastName = last;
+  this.streetName = street;
+  this.cityName = city;
+  this.stateName = state;
+  this.zipCode = zip;
 }
 
 Contact.prototype.fullName = function() {
@@ -15,8 +19,12 @@ $(document).ready(function() {
 
     var inputtedFirstName = $("#new-first-name").val();
     var inputtedLastName = $("#new-last-name").val();
+    var inputtedStreet = $("#new-street-name").val();
+    var inputtedCity = $("#new-city-name").val();
+    var inputtedState = $("#new-state-name").val();
+    var inputtedZip = $("#new-zip-name").val();
 
-    var newContact = new Contact(inputtedFirstName, inputtedLastName);
+    var newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedStreet, inputtedCity, inputtedState, inputtedZip);
 
     $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</li>");
 
@@ -25,11 +33,18 @@ $(document).ready(function() {
       $("#show-contact h2").text(newContact.firstName);
       $(".first-name").text(newContact.firstName);
       $(".last-name").text(newContact.lastName);
+      $(".street-name").text(newContact.streetName);
+      $(".city-name").text(newContact.cityName);
+      $(".state-name").text(newContact.stateName);
+      $(".zip-name").text(newContact.zipCode);
     });
 
     $("input#new-first-name").val("");
     $("input#new-last-name").val("");
-
+    $("input#new-street-name").val("");
+    $("input#new-city-name").val("");
+    $("input#new-state-name").val("");
+    $("input#new-zip-name").val("");
 
   });//Submit end
 });//Ready end
